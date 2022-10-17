296ac15bb6518c44fbc380d3c181398b81b10357
Kevin Chen <kc949281@gmail.com>
2022-10-17 23:17:10 +0900

    v1

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -1,11 +1,29 @@
 // チェッカー
 function setup() {
   createCanvas(200, 200);
-  let size = width / 8;
-  noStroke();
+
+//Color Coordinates  
+    let B= color (0,0,0)
+    let W= color (255,255,255)
+
+//Color Fill
+    if((i+j)/2 == 0) {
+      fill (B) 
+
+      else {
+        fill (W)
+      }
+    }
+
+
+//Size Condition    
+
+
+//Checkers Board
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
-      // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      rect(10*j,10*i,10,10);
+
+      }
     }
   }
-}
